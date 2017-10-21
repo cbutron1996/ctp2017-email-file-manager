d6b556cebc0b355a1ed04584088000f7a284a757
Christian Butron <cbutron1996@gmail.com>
2017-10-21 19:34:54 -0400

    Emails controller

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
+router.use('/emails', require('./emails'));
 
 
 module.exports = router;
