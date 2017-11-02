bfa060f9ed19b6210a12cdcca1fd4265f8de3b7d
Christian Butron <cbutron1996@gmail.com>
2017-11-02 00:28:28 -0400

    Replaced Redirect URL + Added gitignore

--- middlewares/google-oauth.js
+++ middlewares/google-oauth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: "434306998415-5f832nmc41pk44u6t6op0nfsp60la88s.apps.googleusercontent.com",
     clientSecret: "49Dwi-UagnZHF-zKfWfRBIu3",
-    callbackURL: "https://github.com/cbutron1996/ctp2017-email-file-manager"
+    callbackURL: "http://localhost:8000"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
