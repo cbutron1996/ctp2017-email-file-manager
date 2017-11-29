07bee551cc04481d39775e57b2844ca7f6aedc5a
Christian Butron <cbutron1996@gmail.com>
2017-11-29 18:51:53 -0500

    Fixed Attachments Page

--- controllers/attachments.js
+++ controllers/attachments.js
@@ -63,7 +63,16 @@ router.get('/', (req, res) => {
       })
     }
   );
-  res.json('Complete???');
+  // res.json('Complete???');
+  Attachments.findAll({
+    where: { user_id: req.user.email }
+  }).then((attachments) => {
+      res.render('file_section', {
+        user: req.user,
+        attachments: attachments,
+      })
+    }
+  );
 });
 
 router.get('/:id', (req, res) => {
@@ -77,17 +86,8 @@ router.get('/:id', (req, res) => {
       return;
     }
     getAttachments(req, response);
-    //res.json('Complete?');
+    res.json('Complete?');
   });
-  Emails.findAll({
-    where: { user_id: req.user.email }
-  }).then((emails) => {
-      res.render('email_section', {
-        user: req.user,
-        emails: emails,
-      })
-    }
-  );
 });
 
 router.get('/:id/:aid', (req, res) => {
